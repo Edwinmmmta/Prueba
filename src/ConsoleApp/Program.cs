752fc5c15d790a8c9da5f1fb47afaa5c44bb5525
Edwin Hernandez <edwin.hernandezt@my.unitec.edu.mx>
2022-10-23 19:38:46 +0000

    Updated Program.cs

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -22,18 +22,38 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            print("calcula el area y perimetro del triangulo");
-            float _base = float(input("Escribe la base "));
-            float altura = float(input("escribe la altura "));
-            float lado1 = float(input("escribe el lado 1 "));
-            float lado2 = float(input("escribe el lado 2 "));
-            float lado3 = float(input("escribe el lado 3 "));
+            Console.WriteLine("Calcula el area y perimetro del triangulo");
+            Console.WriteLine();
+            
+            Console.Write("Escribe la  base: ");
+            float _base = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Escribe la altura: ");
+            float altura = Convert.ToSingle(Console.ReadLine());
+
+           Console.Write("Escribe el lado 1: "); 
+            float lado1 = Convert.ToSingle(Console.ReadLine());
+            
+            Console.Write("Escribe el lado 2: "); 
+            float lado2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Escribe el lado 3: "); 
+            float lado3 = Convert.ToSingle(Console.ReadLine());
+
 
             float perimetro=Application.Triangulo.Perimetro(lado1,lado2,lado3);
             float area=Application.Triangulo.Area(_base, altura);
 
-            print("el area es : ", area);
-            print("el perimetro es : ", perimetro);
+            Console.WriteLine();
+
+            Console.WriteLine("El area es : " + area.ToString("n4"));
+            Console.WriteLine("El perimetro es : "+ perimetro.ToString("n4"));
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Presione cualquier tecla para terminar....");
+            Console.ReadKey();
         }
     }
 }
